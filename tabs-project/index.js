9048a7ded7fd910fda8112c75d5cc61eb6041cd8
gr8goat <yotsathon.rit@gmail.com>
2022-08-31 20:20:14 +0700

    Add tabs project

---
+++ tabs-project/index.js
@@ -0,0 +1,18 @@
+const btns = document.querySelectorAll('.button');
+const tabs = document.querySelector('.tabs');
+const articles = document.querySelectorAll('.content');
+
+tabs.addEventListener('click', (event) => {
+    const id = event.target.dataset.id;
+    if (id) {
+        btns.forEach((btn) => {
+            btn.classList.remove('live');
+        });
+        event.target.classList.add('live');
+        articles.forEach((article) => {
+            article.classList.remove('live');
+        });
+        const element = document.getElementById(id);
+        element.classList.add('live');
+    }
+});
