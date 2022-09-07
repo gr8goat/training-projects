4d9623231fef7d80d7ae7fa449e3546c32a6b7b0
gr8goat <yotsathon.rit@gmail.com>
2022-09-07 18:13:07 +0700

    Finish agency website

--- agency-website/assets/js/script.js
+++ agency-website/assets/js/script.js
@@ -40,3 +40,20 @@ navbarLinks.forEach((elem) => {
         overlay.classList.toggle('active');
     });
 });
+
+/**
+ * header & go-top-btn active when window scroll down to 400px
+ */
+
+const header = document.querySelector('[data-header]');
+const goTopBtn = document.querySelector('[data-go-top]');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 400) {
+        header.classList.add('active');
+        goTopBtn.classList.add('active');
+    } else {
+        header.classList.remove('active');
+        goTopBtn.classList.remove('active');
+    }
+});
